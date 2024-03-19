6f1a62fe68c9841fd77f99dcb286a31ca156626a
Amara <ajbriquelet@gmail.com>
2024-03-19 18:12:00 -0500

    m

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/notes', function(req, res) {
-    // Serve notes.html
-    res.sendFile(path.join(__dirname, 'Develop', 'public', 'notes.html'));
+  res.sendFile(path.join(__dirname, 'Develop', 'public', 'notes.html'));
 });
 
 app.get('/api/notes', function(req, res) {
